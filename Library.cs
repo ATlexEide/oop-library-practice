629bb20364155e947060a082c21a18b70677829f
Alexander Eide <atlexeide@gmail.com>
2024-11-12 22:25:23 +0100

    Move methods

--- Library.cs
+++ Library.cs
@@ -20,47 +20,12 @@ public class Library
         Console.WriteLine($"Lending you {book}");
         return book;
     }
-    public void ListAvailableBooks(){
+       public void ListAvailableBooks(){
         AvailableBooks.ForEach((book) => Console.WriteLine($"{book.Title} by {book.Author}"));
         Console.WriteLine("/////////////////////////");
         Console.WriteLine("Press any key to go back");
         Console.ReadKey();
         openMenu();
     }
-    
-    p
-    public void runOption(int selected){
-      switch (selected)
-      {
-        case 0:
-            Console.Clear();
-            ListAvailableBooks();
-        break;
-        case 1:
-        Console.Clear();
-        Console.WriteLine("Enter book title:");
-        String title = Console.ReadLine();
-        Console.WriteLine("Enter book author:");
-        String author = Console.ReadLine();
-        if(string.IsNullOrWhiteSpace(title) || string.IsNullOrWhiteSpace(author))
-        {
-            Console.WriteLine("Title and author can not be empty");
-            Console.ReadKey();            
-            runOption(selected);
-        }else{
-            AvailableBooks.Add(new Book(title, author));
-            openMenu();
-        }
-            
-        break;
-        case 2:
-       Console.Clear();
-            Console.WriteLine("Hmmm, seems functionality is missing..");
-            Console.WriteLine("/////////////////////////");
-        Console.WriteLine("Press any key to go back");
-        Console.ReadKey();
-        openMenu();
-        break;
-      }
-    }
+
 }
--- LibraryMenu.cs
+++ LibraryMenu.cs
@@ -34,4 +34,39 @@ public class Menu
             return selectOption();
         }
     }
+        public void runOption(int selected){
+      switch (selected)
+      {
+        case 0:
+            Console.Clear();
+            ListAvailableBooks();
+        break;
+        case 1:
+        Console.Clear();
+        Console.WriteLine("Enter book title:");
+        String title = Console.ReadLine();
+        Console.WriteLine("Enter book author:");
+        String author = Console.ReadLine();
+        if(string.IsNullOrWhiteSpace(title) || string.IsNullOrWhiteSpace(author))
+        {
+            Console.WriteLine("Title and author can not be empty");
+            Console.ReadKey();            
+            runOption(selected);
+        }else{
+            AvailableBooks.Add(new Book(title, author));
+            openMenu();
+        }
+            
+        break;
+        case 2:
+       Console.Clear();
+            Console.WriteLine("Hmmm, seems functionality is missing..");
+            Console.WriteLine("/////////////////////////");
+        Console.WriteLine("Press any key to go back");
+        Console.ReadKey();
+        openMenu();
+        break;
+      }
+    }
+ 
 }
